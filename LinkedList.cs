e59a3594a5aa33c619776106abb17d7f4def4fe8
Ankitsehrawat01 <sehrawata328@gmail.com>
2022-08-26 16:31:03 +0530

    linkedlist using Nodes

---
+++ LinkedList.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_LinkedList
+{
+    public class LinkedList
+    {
+        Node head;
+        public void AddNode(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.Next != null)
+                {
+                    temp = temp.Next;
+                }
+                temp.Next = node;
+
+            }
+            Console.WriteLine("{0} inserted into the linkedlist", node.data);
+        }
+        public void Display()
+        {
+            if (head == null)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("The List is empty");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Linked List Contains: ");
+                Console.ResetColor();
+                Node tempnode = head;
+                while (tempnode != null)
+                {
+                    Console.Write(tempnode.data + " ");
+                    tempnode = tempnode.Next;
+                }
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,14 +4,35 @@
     {
         public static void Main(string[] args)
         {
-            LinkedList<int> a1 = new LinkedList<int>();
-            a1.AddLast(56);
-            a1.AddLast(30);
-            a1.AddLast(70);
-            foreach (int data in a1)
+            bool check = true;
+            while (check)
             {
-                Console.WriteLine("| " + data + " |");
-                Console.WriteLine("-----------------");
+                Console.Write("Select Option:\n" +
+                    "1) Simple LinkedList\n" +
+                    "2) LinkedList using Nodes\n");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        LinkedList<int> a1 = new LinkedList<int>();
+                        a1.AddLast(56);
+                        a1.AddLast(30);
+                        a1.AddLast(70);
+                        foreach (int data in a1)
+                        {
+                            Console.WriteLine("-----------------");
+                            Console.WriteLine("| " + data + " |");
+                            Console.WriteLine("-----------------");
+                        }
+                        break;
+                    case 2:
+                        LinkedList a2 = new LinkedList();
+                        a2.AddNode(56);
+                        a2.AddNode(30);
+                        a2.AddNode(70);
+                        a2.Display();
+                        break;
+                }
             }
         }
     }
