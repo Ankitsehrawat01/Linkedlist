44c7446d64e0d33e1430c02adfc472d1ce16282c
Ankitsehrawat01 <sehrawata328@gmail.com>
2022-08-26 15:54:35 +0530

    Simple linkedlist

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Simple_LinkedList
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            LinkedList<int> a1 = new LinkedList<int>();
+            a1.AddLast(56);
+            a1.AddLast(30);
+            a1.AddLast(70);
+            foreach (int data in a1)
+            {
+                Console.WriteLine("| " + data + " |");
+                Console.WriteLine("-----------------");
+            }
+        }
+    }
+}
